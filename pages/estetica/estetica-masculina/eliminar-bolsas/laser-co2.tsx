1289a527205b6d66149ffa3679894a54d8316bf5
nve-versefi <n.vaneerde@versefi.co.uk>
2024-06-11 11:31:58 +0100

    Added missing pages

---
+++ pages/estetica/estetica-masculina/eliminar-bolsas/laser-co2.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import ServiceInfo from '@/components/serviceinfo';
+import DefaultLayout from '@/app/(default)/layout';
+import RootLayout from '@/app/layout';
+
+const Home: React.FC = () => {
+  const [serviceData, setServiceData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchServiceData = async () => {
+      try {
+        const encodedTitle = encodeURIComponent('Láser CO2');
+        const encodedObjectives = encodeURIComponent('eliminar-bolsas');
+        const encodedSubcategory = encodeURIComponent('estetica-masculina');
+        const response = await fetch(`/api/services?title=${encodedTitle}&objectives=${encodedObjectives}&subcategory=${encodedSubcategory}`);
+        
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Fetched service data:', data);
+          setServiceData(data.services[0]);
+        } else {
+          console.error('Failed to fetch service:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Failed to fetch service:', error);
+      }
+    };
+    fetchServiceData();
+  }, []);
+
+  return (
+    <>
+      <RootLayout>
+        <DefaultLayout>
+          <ServiceInfo serviceData={serviceData} />
+        </DefaultLayout>
+      </RootLayout>
+    </>
+  );
+};
+
+export default Home;
