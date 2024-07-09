e05953b1209bdef6d0b1f508cc307800734b5c0a
nve-versefi <n.vaneerde@versefi.co.uk>
2024-07-09 20:00:07 +0100

    Improved Searchbar

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -14,13 +14,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const db = client.db(process.env.DB_NAME);
       const collection = db.collection('Tratamientos');
 
-      const searchResults = await collection.find({
-        $or: [
-          { title: { $regex: q as string, $options: 'i' } },
-          { subcategory: { $regex: q as string, $options: 'i' } },
-          { 'objectives': { $regex: q as string, $options: 'i' } }
-        ]
-      }).limit(10).toArray();
+      const searchResults = await collection.find(
+        {
+          $or: [
+            { title: { $regex: q as string, $options: 'i' } },
+            { subcategory: { $regex: q as string, $options: 'i' } },
+            { objectives: { $regex: q as string, $options: 'i' } }
+          ]
+        },
+        {
+          projection: {
+            _id: 1,
+            title: 1,
+            subcategory: 1,
+            objectives: 1
+          }
+        }
+      ).limit(10).toArray();
 
       res.status(200).json(searchResults);
     } catch (error: unknown) {
