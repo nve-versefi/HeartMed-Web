d236f1b28179b2c7d2c0c0e88b64eaeb9709cb4e
nve-versefi <n.vaneerde@versefi.co.uk>
2024-07-11 10:36:24 +0100

    Standarized Margins in Service Page Template

--- components/serviceinfo.tsx
+++ components/serviceinfo.tsx
@@ -54,7 +54,7 @@ const ServiceInfo: React.FC<ServiceInfoProps> = ({ serviceData }) => {
           })}
         </script>
       </Head>
-    <div className="container mx-auto">
+    <div className="container mx-auto px-4">
       <div className="banner-container">
         <div className="w-screen h-64 overflow-hidden relative">
           <div
@@ -67,45 +67,42 @@ const ServiceInfo: React.FC<ServiceInfoProps> = ({ serviceData }) => {
       </div>
       <div className="container mx-auto">
         <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8 mb-4">
-          <div className="ml-8">
+          <div className="md:ml-8">
             <h2 className="text-2xl text-thunderbird-500 font-bold mb-4">{serviceData.subtitle1 ?? 'Subtitulo 1'}</h2>
             <p className="text-lg text-woodsmoke-700 font-regular">{renderTextWithLineBreaks(serviceData.what)}</p>
           </div>
 
-          <div className="mr-12">
+          <div className="md:mr-12">
             <h2 className="text-2xl text-thunderbird-500 font-bold mb-4">{serviceData.subtitle2 ?? 'Subtitulo 2'}</h2>
             <p className="text-lg text-woodsmoke-700 font-regular">{renderTextWithLineBreaks(serviceData.how)}</p>
           </div>
         </div>
         <div className="mt-12">
             <h2 className="text-2xl text-center text-thunderbird-500 font-bold mb-4">{serviceData.subtitle3 ?? 'Subtitulo 3'}</h2>
-            <p className="text-lg text-woodsmoke-700 font-regular">{renderTextWithLineBreaks(serviceData.area)}</p>
+            <p className="text-lg text-woodsmoke-700 font-regular text-center">{renderTextWithLineBreaks(serviceData.area)}</p>
         </div>
         <div className="mb-4 text-center">
           <p className="text-lg">{renderTextWithLineBreaks(serviceData.objective1)}</p>
           <p className="text-lg">{renderTextWithLineBreaks(serviceData.objective2)}</p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-  <div className="flex justify-center" style={{ height: '450px', width: 'auto' }}>
-    <img
-      src={serviceData.image2 || "/mnt/data/image.png"}
-      alt="Service Image 1"
-      style={{ width: '450px', height: '450px', objectFit: 'cover' }}
-    />
-  </div>
-  <div className="flex justify-center" style={{ height: '450px', width: 'auto' }}>
-    <img
-      src={serviceData.image3 || "/mnt/data/image.png"}
-      alt="Service Image 2"
-      style={{ width: '450px', height: '450px', objectFit: 'cover' }}
-    />
-  </div>
-</div>
-
-
-
+          <div className="flex justify-center" style={{ height: '450px', width: 'auto' }}>
+            <img
+              src={serviceData.image2 || "/mnt/data/image.png"}
+              alt="Service Image 1"
+              style={{ width: '450px', height: '450px', objectFit: 'cover' }}
+            />
+          </div>
+          <div className="flex justify-center" style={{ height: '450px', width: 'auto' }}>
+            <img
+              src={serviceData.image3 || "/mnt/data/image.png"}
+              alt="Service Image 2"
+              style={{ width: '450px', height: '450px', objectFit: 'cover' }}
+            />
+          </div>
+        </div>
         <div className="p-6 my-8">
-          <div className="grid grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div className="text-center">
               <FaHourglassHalf className="text-4xl text-thunderbird-500 mx-auto mb-4" />
               <p className="text-gray-700">
